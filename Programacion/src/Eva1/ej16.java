ec87f3498e53c34f365accca4b0fce8c2541f551
Andrei <Andrei@DESKTOP-I6PLB5U.localdomain>
2020-10-26 12:39:30 +0100

    +ej16

---
+++ Programacion/src/Eva1/ej16.java
@@ -0,0 +1,25 @@
+package Eva1;
+
+import java.util.Scanner;
+
+public class ej16 {
+
+	public static void main(String[] args) {
+		int cuenta=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ponme desde donde quieres que empiece a contar");
+		int num1=sc.nextInt();
+		System.out.println("Ponme donde quieres que acabe:");
+		int num2=sc.nextInt();
+		System.out.println("Ponme de cuantos en cuanto quieres que cuente");
+		int intervalo=sc.nextInt();
+		cuenta=num1;
+		while (cuenta<=num2) {
+			System.out.println(cuenta);
+			cuenta+=intervalo;
+			
+		}
+
+	}
+
+}
