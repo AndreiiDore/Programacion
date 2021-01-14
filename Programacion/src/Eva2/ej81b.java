05cad94255285f9ab2181bd4acf7071ec8819132
andre <andre@MSI.home>
2021-01-14 10:02:40 +0100

    ej 81 a,b,c

--- Programacion/src/Eva2/ej81b.java
+++ Programacion/src/Eva2/ej81b.java
@@ -3,22 +3,18 @@ package Eva2;
 public class ej81b {
 
 	public static void main(String[] args) {
-		intervalo(1,10);
+		int resultado [] =intervalo(1,10);
+		for (int i = 0; i < resultado.length; i++) {
+			System.out.println(resultado[i]);
+		}
 
 	}
-	private static void intervalo (int num1,int num2){
-		int cont=0;
-		for (int i = num1; i <= num2; i++) {
-			cont++;
+	private static int[] intervalo (int num1,int num2){
+		int array []=new int [num2-num1];
+		for (int i = num1, j=0; i < num2; i++,j++) {
+			array[j]=i;
 		}
-		int x=0;
-		int array []=new int [cont];
-		for (int i = num1; i < num2; i++) {
-			array[x]=i;
-			System.out.println("|"+array[x]+"|");
-			x++;
-			
-		}
+		return array;
 	}
 
 }
---
+++ Programacion/src/Eva2/ej81c.java
@@ -0,0 +1,19 @@
+package Eva2;
+
+public class ej81c {
+
+	public static void main(String[] args) {
+		int resultado []=intervalo(0,10,2);
+		for (int i = 0; i < resultado.length; i++) {
+			System.out.println(resultado[i]);
+		}
+
+	}
+	private static int[] intervalo (int num1,int num2,int intervalo){
+		int array []=new int [(num2-num1)/intervalo+1];
+		for (int i = num1, j=0; i <= num2; i=i+intervalo,j++) {
+			array[j]=i;
+		}
+		return array;
+	}
+}
