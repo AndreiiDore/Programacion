3204850125fb9670d530d3f9ab47cb2c20662016
Andrei <Andrei@DESKTOP-I6PLB5U.localdomain>
2021-01-25 10:01:39 +0100

    EJ22_Libro Comentaduuuu

--- Programacion/src/ej22/Libro.java
+++ Programacion/src/ej22/Libro.java
@@ -1,20 +1,37 @@
 package ej22;
-
+ 	/**
+ 	 * 
+ 	 * @author Andrei
+ 	 *
+ 	 */
 public class Libro {
 	private String Titulo;
 	private String Autor;
 	private int numEjemplares;
 	private int numPrestados;
 	
-	public Libro() {
-		
-	}
+	/**
+	 * Constructor libro vacio
+	 */
+	public Libro() {}
+	/**
+	 * Constructor libro en el cual tenemos los siguientes parametros:
+	 * @param titulo Nombre del Libro
+	 * @param autor Nombre del Autor
+	 * @param numeroEjemplares Numeros de los ejemplares que tenemos en stock
+	 * @param numPrestados Numero de libros que hemos prestado de un dicho libro
+	 */
 	public Libro(String titulo,String autor,int numeroEjemplares,int numPrestados) {
+
 		this.Titulo=titulo;
 		this.Autor=autor;
 		this.numEjemplares=numeroEjemplares;
 		this.numPrestados=numPrestados;
 	}
+	/**
+	 * Metodo prestamo en el cual vamos a ver si un libro se puede prestar, en mi caso he puesto dos
+	 * mensajes por pantalla para saber que lo que hago se hace xD
+	 */
 	public void prestamo() {
 		if (numEjemplares>numPrestados) {
 			numPrestados++;
@@ -23,6 +40,10 @@ public class Libro {
 			System.out.println("No se puede prestar el libro por falta de Ejemplares");
 		}
 	}
+	/**
+	 * El metodo Devolver lo que hace es que si tenemos algun libro prestado, nos de la opcion de devolver este mismo,
+	 * tocando asi la variable numPestados
+	 */
 	public void devolver() {
 
 		if (numPrestados==0) {
@@ -33,6 +54,17 @@ public class Libro {
 		}
 	}
 	
+	/**
+	 * Aqui ponemos el override porque sustituye al metodo toString de la clase String
+	 * y es para que aparezca comentada dado a que un futuro cuando se den cosas mas complejas
+	 * se necesitara ese "comentario" para saber que estamos sustituyendo
+	 * 
+	 * En el metodo creado, vamos a devolver los datos de un libro, es decir,
+	 * Nombre del Libro
+	 * Autor del Libro
+	 * Numeros de ejemplares que tenemos en "Stock"
+	 * Numeros de libros prestados
+	 */
 	@Override
 	public String toString() {
 		return "Titulo del Libro: "+getTitulo()+"\nNombre del autor: "+getAutor()+"\nEjemplares del libro: "+getNumEjemplares()+"\nLibros prestados: "+getNumPrestados();
