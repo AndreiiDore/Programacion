3b91c24330d81bb0471c01af34ad6c260340391a
andre <andre@MSI>
2021-02-08 19:10:23 +0100

    spoty asaber que se hace

--- Programacion/src/ej25/Ej25.java
+++ Programacion/src/ej25/Ej25.java
@@ -1,12 +1,17 @@
 package ej25;
+import java.util.Scanner;
 
-public class Ej25 {
+public class Ej25 extends Playlist {
 
+	Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		Cancion cancion1=new Cancion("100","Anuel",3.35);
 		System.out.println(cancion1.reproducir());
-		aniadirCancion();
+		
+		aniadirCancion(,Cancion cancion1);
 
 	}
+	
 
 }
--- Programacion/src/ej25/Playlist.java
+++ Programacion/src/ej25/Playlist.java
@@ -6,8 +6,40 @@ public class Playlist extends Cancion{
 	/**
 	 * ArrayList llamado playlist para ir metiendo canciones alli;
 	 */
-	ArrayList<Cancion> playlist = new ArrayList();
-	public void aniadirCancion() {
-		
+	/**
+	 * Vamos a poner de parametros de la clase:
+	 * @param nombrePlaylist: Nombre de la playlist que le vamos a poner
+	 */
+	
+	private String nombrePlaylist;
+	private ArrayList<Cancion> playlist = new ArrayList();
+	
+	public Playlist() {}
+	public Playlist(String Nombre) {
+		this.nombrePlaylist=Nombre;
+		setNombrePlaylist(Nombre);
 	}
+	public String aniadirCancion  (String mensaje, Cancion cancion) {
+		playlist.add(playlist.size(),cancion);
+		return "Cancion añadida con exito";
+	}
+	
+	
+	public ArrayList<Cancion> getPlaylist() {
+		return playlist;
+	}
+
+	public void setPlaylist(ArrayList<Cancion> playlist) {
+		this.playlist = playlist;
+	}
+
+	public String getNombrePlaylist() {
+		return nombrePlaylist;
+	}
+
+	public void setNombrePlaylist(String nombrePlaylist) {
+		this.nombrePlaylist = nombrePlaylist;
+	}
+
+	
 }
